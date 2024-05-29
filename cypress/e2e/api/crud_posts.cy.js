6036ded55b2c39340c272638f14f115adcd92799
Eliel <eliel.o.dantas@outlook.com>
2024-05-29 08:56:59 -0300

    [FEAT] Variáveis de ambiente, Cookies, jwt, Commands e Teste de Contrato pt1

--- cypress/e2e/api/crud_posts.cy.js
+++ cypress/e2e/api/crud_posts.cy.js
@@ -1,13 +1,16 @@
 describe('CRUD - Post', () => {
 
+    let postID = ''
+    let msg = 'Este post foi feito pelo Cypress'
+
     before(() => {
 
         cy.request({
             method: 'POST',
             url: '/store/order',
             body: {
-                id: 7878787878807,
-                petId: 0,
+                id: Cypress.env('id'),
+                petId: Cypress.env('petId'),
                 quantity: 0,
                 shipDate: '2023-11-27T01:54:59.208+0000',
                 status: 'placed',
@@ -16,8 +19,39 @@ describe('CRUD - Post', () => {
         })
     })
 
-    it('teste', () => {
+    //SOMENTE UM EXEMPLO de JWT - cookies
+    it('cria um post', () => {
 
-        cy.log('teste')
+        cy.request({
+            method: 'POST',
+            url: '/api/posts',
+            body: {
+                text: 'Este post foi feito pelo Cypress'
+            }
+        }).then (( { body} ) => {
+            expect(status).to.eq(201)
+        })
+    })
+
+    //EXEMPLO do método POST
+    it('atualiza o post', () => {
+        
+        cy.request ({
+            method: 'PUT',
+            url: `/api/posts/like/${postID}`
+        }).then (({ status }) => {
+            expect(status).to.eq(200)
+        })
+    })
+
+    it('deleta o post', () => {
+        
+        cy.request({
+            method: 'DELETE',
+            url: `/api/posts/${postID}`
+        }).then(({ status }) => {
+            expect(status).to.eq(200)
+            expect(body.msg).to.eq('Post Removido')
+        })
     })
 })
